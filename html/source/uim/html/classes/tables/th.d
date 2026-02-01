22f21fdf5d95c50435bf5ae7049b917e26940840
UIMSolutions <ui.manufaktur@gmail.com>
2026-02-01 12:12:29 +0100

    Update copyright information across multiple files to reflect the new naming convention for the author, changing "UIManufaktur" to "UI-Manufaktur UG *R.I.P*". This includes updates in footer, header, hgroup, hr, img, interfaces, link, main, nav, p, package, search, section, small, span, and various form and list elements, as well as table-related files. Ensure consistency in licensing terms and author attribution throughout the codebase.

--- html/source/uim/html/classes/tables/th.d
+++ html/source/uim/html/classes/tables/th.d
@@ -1,9 +1,9 @@
 module uim.html.classes.tables.th;
 
 /****************************************************************************************************************
-* Copyright: © 2018-2026 Ozan Nurettin Süel (aka UIManufaktur) 
+* Copyright: © 2018-2026 Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*) 
 * License: Subject to the terms of the Apache 2.0 license, as written in the included LICENSE.txt file. 
-* Authors: Ozan Nurettin Süel (aka UIManufaktur)
+* Authors: Ozan Nurettin Süel (aka UI-Manufaktur UG *R.I.P*)
 *****************************************************************************************************************/
 
 import uim.html;
